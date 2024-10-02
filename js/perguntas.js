ddb26247406ce94dba51e20cd876c586c8a71e7a
Joseph <129787928+JosephTHDVy@users.noreply.github.com>
2024-10-01 21:37:23 -0300

    1

--- js/perguntas.js
+++ js/perguntas.js
@@ -5,19 +5,97 @@ export const perguntas = [
             {
                 texto: "Isso é assustador!",
                 afirmacao: [
-                    "No início ficou com medo do que essa tecnologia pode fazer.",
-                    "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
+                    "No início ficou com medo do que esse cara faria com seu botão"
                 ],
                 proxima: 1,
             },
             {
                 texto: "Isso é maravilhoso!",
                 afirmacao: [
-                    "Quis saber como usar IA no seu dia a dia.",
-                    "Pensou que IA pode ajudar em tarefas da sua vida."
+                    "Pensou que essa era uma oportunidade de fazer um novo amigo."
                 ],
                 proxima: 2,
             },
         ]
-    }
-]
+    },
+    {
+        enunciado: "Ao caminho da escola você se encontra com uma oportunidade de aprender a linguagem de programação Java Script, você aceita?",
+        alternativas: [
+            {
+                texto: "Utiliza uma linguagem nova parece legal.",
+                afirmacao: [
+                    "Além de tudo, descobriu um imenso desejo de programar em JS(o que é muito gay)"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Não *chuta o homem",
+                afirmacao: [
+                    "Provou sua masculinidade, recusando as tentações do mundo colorido."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao chegar no colégio seu professor te convida a fazer um acordo",
+        alternativas: [
+            {
+                texto: "Não vejo porque não.",
+                afirmacao: [
+                    "Além de ter uma ingênuidade que te deixa desprevinido."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "IIIIIIIHH SARTEX!",
+                afirmacao: [
+                    "E mesmo com todas as tentações e adiversidades prova-se masculo e astuto😎"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Ferramentas de busca que utilizam IA podem facilitar muito nosso processo de busca por informações. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Utilizar informações próprias traz um toque pessoal ao nosso trabalho. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+];
